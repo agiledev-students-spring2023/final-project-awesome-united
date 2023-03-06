543acdf123735619959f37235d350c9cdf380124
sirbobthemarvelous <robzheng3@gmail.com>
2023-03-06 13:28:05 -0500

    Adding Packages for React
    adding React-router-dom and react-burger-menu

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { slide as Menu } from "react-burger-menu"
 
 function App() {
   return (
@@ -17,6 +20,36 @@ function App() {
         >
           Learn React
         </a>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/animals" element={<AnimalsList />} />
+            <Route path="/animals/:id" element={<Animal />} />
+            <Link to="/animals">See all animals</Link> {/*the preferred way to  link*/}
+          </Routes>
+          <Footer />
+        </Router>
+
+
+        {/*
+        useEffect(() => {
+          async function fetchData() {
+            const result = await axios {
+              //retrieve mock data about animals
+              "https://my.api.mockaroo.com/animals.json?key=d9ddfc40"
+            }
+          }
+          setData(result.data);
+        })
+        fetchData()
+        //create loop to make an object for every instance
+        data.map(item=> (
+          console.log(item);
+        ))
+        
+      */}
       </header>
     </div>
   );

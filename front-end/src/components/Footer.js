88a4721d877b6a29e02922900ffe310f3466a2f6
Ashwin Rabindran <ashwin300@gmail.com>
2023-03-08 13:20:02 -0500

    format code correctly

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,41 +1,39 @@
-import React from "react"
-import "./Footer.css"
-import HomeIcon from '@mui/icons-material/Home';
-import SettingsIcon from '@mui/icons-material/Settings';
+import React from "react";
+import "./Footer.css";
+import HomeIcon from "@mui/icons-material/Home";
+import SettingsIcon from "@mui/icons-material/Settings";
 import { Grid } from "@mui/material";
-import AddHomeWorkIcon from '@mui/icons-material/AddHomeWork';
-import IconButton from '@mui/material/IconButton';
-import Discover from "../pages/Discover/Discover";
-import { useNavigate } from "react-router-dom";
+import AddHomeWorkIcon from "@mui/icons-material/AddHomeWork";
+import IconButton from "@mui/material/IconButton";
 import { Link } from "react-router-dom";
 
-const Footer = props => {
-   
-    
-    return (
-       
-      <div className="footer">
-        <Grid container justifyContent="space-around" direction="row" spacing={.5} xs={11.3}>
-            <Link to="/discover">
-            <IconButton >
-                <HomeIcon className="footerButton" fontSize="60px" />
-            </IconButton>
-             </Link>
-        <Link to="/matches">   
-        <IconButton >
+const Footer = (props) => {
+  return (
+    <div className="footer">
+      <Grid
+        container
+        justifyContent="space-around"
+        direction="row"
+        spacing={0.5}
+        xs={11.3}
+      >
+        <Link to="/discover">
+          <IconButton>
+            <HomeIcon className="footerButton" fontSize="60px" />
+          </IconButton>
+        </Link>
+        <Link to="/matches">
+          <IconButton>
             <AddHomeWorkIcon className="footerButton" fontSize="60px" />
-        </IconButton>
+          </IconButton>
         </Link>
-        <Link to="/generalSettings">   
-        <IconButton >
+        <Link to="/generalSettings">
+          <IconButton>
             <SettingsIcon className="footerButton" fontSize="60px" />
-        </IconButton>
+          </IconButton>
         </Link>
-         </Grid>
-   
-      </div>
-  
-    )
-  }
-  
-  export default Footer
+      </Grid>
+    </div>
+  );
+};
+export default Footer;

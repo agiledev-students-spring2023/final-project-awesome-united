f62f69444dc767c90936b6d8876aa8577a5b3b55
sirbobthemarvelous <robzheng3@gmail.com>
2023-03-22 12:11:12 -0400

    rather than subpages they are components
    rather than subpages they are components

--- front-end/src/pages/GeneralSettings/GeneralSettings.js
+++ front-end/src/pages/GeneralSettings/GeneralSettings.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
+import SizeSettings from "../../components/SizeSettings";
+import BrightnessSettings from "../../components/BrightnessSettings";
+import AccountInfo from "../../components/AccountInfo";
 
 // set up routes so different URL routes load up different main components
 const GeneralSettings = (props) => {
@@ -9,15 +11,9 @@ const GeneralSettings = (props) => {
       <h1>
         General Settings Page
       </h1>
-      <Link to="/brightnessSettings">
-      Change Brightness
-      </Link>
-      <Link to="/sizeSettings">
-      Change Size of Text
-      </Link>
-      <Link to="/accountInfo">
-      See Account Info
-      </Link>
+      <BrightnessSettings />
+      <SizeSettings />
+      <AccountInfo />
       <p>Change your Password - linked page
       Change Brightness
       Change size of Text - linked page

b161fb6ed5012d8c34863a02ca7e3a1f86a51bf0
sirbobthemarvelous <robzheng3@gmail.com>
2023-03-22 21:06:50 -0400

    lower scope of settings page
    https://github.com/agiledev-students-spring-2023/final-project-awesome-united/pull/35

--- front-end/src/pages/GeneralSettings/GeneralSettings.js
+++ front-end/src/pages/GeneralSettings/GeneralSettings.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SizeSettings from "../../components/SizeSettings";
-import BrightnessSettings from "../../components/BrightnessSettings";
+//import SizeSettings from "../../components/SizeSettings";
+//import BrightnessSettings from "../../components/BrightnessSettings";
 import AccountInfo from "../../components/AccountInfo";
 
 // set up routes so different URL routes load up different main components
@@ -11,13 +11,8 @@ const GeneralSettings = (props) => {
       <h1>
         General Settings Page
       </h1>
-      <BrightnessSettings />
-      <SizeSettings />
       <AccountInfo />
       <p>Change your Password - linked page
-      Change Brightness
-      Change size of Text - linked page
-      Enable Dark StrictMode - button
       Account Info - linked
       Log Out - button
       </p>

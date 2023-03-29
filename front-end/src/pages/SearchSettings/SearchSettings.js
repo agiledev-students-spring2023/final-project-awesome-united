135d229f955ec160a6ac4e68d7b50ae1c0161464
ian41284128 <ian4128@nyu.edu>
2023-03-29 00:55:29 -0400

    linked all form input types to state variables

--- front-end/src/pages/SearchSettings/SearchSettings.js
+++ front-end/src/pages/SearchSettings/SearchSettings.js
@@ -1,48 +1,69 @@
+import { useScrollTrigger } from '@mui/material';
+import { useState, useEffect } from "react"
 import './SearchSettings.css';
 
 const SliderOption = ({name="Option", min=0, max=10, step=1}) => {
+  const [minimum, setMinimum] = useState(min);
+  const [maximum, setMaximum] = useState(max);
+
   return (
     <div className="Option">
       <p className="Option-name">{name}</p>
-      <input type="number" className="Input Range Min" min={min} max={max} step={step}></input>
+      <input type="number" className="Input Range Min" min={min} max={max} step={step} 
+      value={minimum} onChange={e => setMinimum(e.target.value)}></input>
       <span className="Range-dash">-</span>
-      <input type="number" className="Input Range Max" min={min} max={max} step={step}></input>
+      <input type="number" className="Input Range Max" min={min} max={max} step={step}
+      value={maximum} onChange={e => setMaximum(e.target.value)}></input>
     </div>
   );
 }
 
 const GridOption = ({name="Option", options}) => {  //options is an array of strings
   const optionComponents = [];
-  options.forEach(option => {
-    optionComponents.push(
+  options.forEach(optionName => {optionComponents.push(<Checkbox option={optionName} key={optionName}/>);});
+
+  return (
+    <div className="Option">
+      <p className="Option-name">{name}</p>
+      <div className="Grid-form">{optionComponents}</div>
+    </div>
+  );
+}
+
+const Checkbox = ({option}) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    console.log(option + " is " + !checked);
+    setChecked(!checked);
+  }
+
+  return (
       <div className="Grid-option">
         <input type="checkbox" className="Input"
           id={(str => {       //convert name to camelcase
             return str.toString().replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => {
               return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
             }).replace(/\s+/g, '');
-          })({option})}   //pass option into anonymous camelcase function}
-          name={option}>
+          })({option})}   //pass option into anonymous camelcase function
+          name={option}
+          value={checked}
+          onChange={handleChange}>
         </input>
         <label>{option}</label>
       </div>
-    );
-  });
-
-  return (
-    <div className="Option">
-      <p className="Option-name">{name}</p>
-      <form className="Grid-form">{optionComponents}</form>
-    </div>
   );
 }
 
-const Option = ({name="Option", type="text", unit=""}) => {
+const Option = ({name="Option", type="text", unit="", def}) => {
+  const [option, changeOption] = useState(def);
+
   return(
     <div className="Option">
       <p className="Option-name">{name}</p>
-      <input className="Input" type={type} id={name.replace(/\s/g, '')} unit={unit}></input>
-      <span>{unit}</span>
+      <input className="Input" type={type} id={name.replace(/\s/g, '')} unit={unit} 
+      onChange={e => changeOption(e.target.value)}></input>
+      <span> {unit}</span>
     </div>
   );
 }
@@ -55,17 +76,17 @@ function SearchSettings() {
           Search Settings
         </h1>
       </header>
-      <div className="Options">
+      <form className="Options">
         <SliderOption name="Price Range" min={0} max={1000}/>
         <GridOption name="Types of Home" options={["Bungalow", "House", "Shack", "Apartment", "Mansion", "Cabin"]}/>
         <GridOption name="Accommodations" options={["Kitchen", "Yard", "Laundry", "Balcony"]}/>
-        <Option name="Search Location"/>
-        <Option name="Distance from Location" type="number" unit="miles"/>
+        <Option name="Search Location" default=""/>
+        <Option name="Distance from Location" type="number" unit="miles" def={5}/>
         <SliderOption name="Number of Beds"/>
         <SliderOption name="Number of Bathrooms"/>
         <br/><br/>
         <input className="Input" type="submit" value="Save"></input>
-      </div>
+      </form>
     </div>
   );
 }

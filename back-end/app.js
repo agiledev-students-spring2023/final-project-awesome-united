4916b70cd0a386d2a990bda2c984c022dfa95378
ian41284128 <ian4128@nyu.edu>
2023-04-02 17:35:54 -0400

    committing changes before updating

--- back-end/app.js
+++ back-end/app.js
@@ -7,13 +7,13 @@ const morgan = require("morgan") // middleware for nice logging of incoming HTTP
 // ngrok is a tool to output a public URL for the webserver running locally 
 const multer = require('multer') // extract files from requests
 
-const path = require("path");
+const path = require('path')
 
 // we will put some server logic here later...
 app.use(morgan("dev")) // dev style gives a concise color-coded style of log output
 app.use(express.json()) // decode JSON-formatted incoming POST data
 app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
-app.use(express.static("../front-end/public"))
+// app.use(express.static("../front-end/public"))
 
 // custom middleware - first
 app.use((req, res, next) => {
@@ -39,8 +39,9 @@ app.get("/middleware-example", (req, res) => {
     res.send(message)
   })
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../front-end/public", "index.html"));
+app.use(express.static(path.join(__dirname, '../front-end/build')))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front-end/build/index.html'))
 })
 
 // export the express app we created to make it available to other modules
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,7 +11,6 @@ import CreateListing from "./pages/CreateListing/CreateListing"
 import Login from "./pages/Login/Login"
 import Logout from "./pages/Logout/Logout"
 import SearchSettings from "./pages/SearchSettings/SearchSettings"
- import Footer from "./components/Footer"
 import "./App.css"
 
 // set up routes so different URL routes load up different main components

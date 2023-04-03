f8a37619515aee040c9dd335c26b11e454463f3a
ian41284128 <ian4128@nyu.edu>
2023-04-02 23:31:34 -0400

    no changes

--- back-end/app.js
+++ back-end/app.js
@@ -43,6 +43,8 @@ app.use(express.static(path.join(__dirname, '../front-end/build')))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../front-end/build/index.html'))
 })
+app.get('/')
+
 
 app.post("/post-search-settings", (req, res) => {
   res.send("saved user data");

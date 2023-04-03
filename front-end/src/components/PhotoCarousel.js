96de08fceb4fad48b52473bb66dc2b8ab9b8d04d
ashwin300 <ashwin300@gmail.com>
2023-04-03 18:26:56 -0400

    fix cara bug

--- front-end/src/components/PhotoCarousel.js
+++ front-end/src/components/PhotoCarousel.js
@@ -5,14 +5,18 @@ function PhotoCarousel(props) {
   const images = props.images;
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const handlePrevClick = () => {
-    const newIndex = currentImageIndex + 1;
-    setCurrentImageIndex(newIndex >= images.length ? 0 : newIndex);
-  };
-
-  const handleNextClick = () => {
+  const handlePrevClick = (event) => {
     const newIndex = currentImageIndex - 1;
     setCurrentImageIndex(newIndex < 0 ? images.length - 1 : newIndex);
+    console.log(currentImageIndex);
+    event.preventDefault();
+  };
+
+  const handleNextClick = (event) => {
+    const newIndex = currentImageIndex + 1;
+    setCurrentImageIndex(newIndex >= images.length ? 0 : newIndex);
+    console.log(currentImageIndex);
+    event.preventDefault();
   };
   if (props.mode === "full") {
     return (

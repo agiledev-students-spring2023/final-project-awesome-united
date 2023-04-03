743debfd15c9ea2f272b115a5c0b4dcdc34c8c67
ashwin300 <ashwin300@gmail.com>
2023-04-03 18:28:22 -0400

    mockaroo comment

--- front-end/src/pages/Discover/Discover.js
+++ front-end/src/pages/Discover/Discover.js
@@ -36,7 +36,7 @@ const Discover = (props) => {
   useEffect(() => {
     async function fetchData() {
       const response = await axios(
-        "https://my.api.mockaroo.com/listing.json?key=a8b2f1e0"
+        "https://my.api.mockaroo.com/listing!.json?key=a8b2f1e0"
       )
         .then((response) => {
           setListings(response.data);

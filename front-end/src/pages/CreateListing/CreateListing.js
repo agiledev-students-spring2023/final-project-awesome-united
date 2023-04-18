8c54cb78572f24b2f7993a675b733857c66967b8
27yama <dlwnsgur109@gmail.com>
2023-04-17 23:16:29 -0400

    Initialize so the page runs properly

--- front-end/src/pages/CreateListing/CreateListing.js
+++ front-end/src/pages/CreateListing/CreateListing.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { TextField, Grid, Button, Typography } from "@mui/material";
-import "./CreateAccount.css";
 import DiscoverHeader from "../../components/DiscoverHeader";
 import { Link } from "react-router-dom";
-import axios from "axios";s
+import axios from "axios";
 
 const CreateListing = props => {
     return (

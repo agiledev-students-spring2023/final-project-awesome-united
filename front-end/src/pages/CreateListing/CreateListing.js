c11c33c01c8528c4ff6f73c23b504b11195a2f78
27yama <dlwnsgur109@gmail.com>
2023-04-17 23:37:29 -0400

    Added and fixed CSS for headers

--- front-end/src/pages/CreateListing/CreateListing.js
+++ front-end/src/pages/CreateListing/CreateListing.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./CreateListing.css"
 import { useForm } from "react-hook-form";
 import { TextField, Grid, Button, Typography } from "@mui/material";
 import DiscoverHeader from "../../components/DiscoverHeader";
@@ -7,9 +8,15 @@ import axios from "axios";
 
 const CreateListing = props => {
     return (
-      <h1>
-        Create Listing
-      </h1>
+      <div className="listingPage">
+      <Typography variant="h6" className="headerText">
+      Property Listing Address <br></br>
+
+      Images <br></br>
+
+      Listing Information
+      </Typography>
+      </div>
     )
   }
   

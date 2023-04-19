98fffb8eee22e906b2ef0658a50289a94141382a
David Acevedo <acevedodavida16@gmail.com>
2023-04-18 20:07:01 -0400

    comment cleanup

--- back-end/models/Matches.js
+++ back-end/models/Matches.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const MatchesSchema = new mongoose.Schema(
     {
-        // array that holds the matches id
+        // array that holds the matches
         members: {
           type: Array,
         },

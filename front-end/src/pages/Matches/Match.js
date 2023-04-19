22fd2c490e38bd4c4da7bb2cca2bba14721fc806
David Acevedo <acevedodavida16@gmail.com>
2023-04-19 03:35:58 -0400

    retrieve acct (name) associated with an id. WIP

--- front-end/src/pages/Matches/Match.js
+++ front-end/src/pages/Matches/Match.js
@@ -1,6 +1,23 @@
+import { useState } from "react"
 import "./Match.css"
+import axios from "axios"
 
 const Match = props => {
+
+
+  //fetch name associated with user-id
+  const [user,setUser] = useState(null)
+
+  useEffect(()=>{
+    // if element (id) in member not equals current logged in user id,
+    //
+    const otherId = props.match.member.find(m => m !== props.currentUserId)
+    const getUser = async ()=>{
+      const res = await axios("/users")
+    } 
+  },[])
+
+
   // this component expects to receive 'name', 'chatPreview', and 'handleClick' values passed as arguments to it
   // react automatically bundles these arguments into an object called props
   return (

c6e6c08c308af49420bed45cb85a82625751cf27
27yama <dlwnsgur109@gmail.com>
2023-04-23 15:57:18 -0400

    Fixed minor UIs

--- front-end/src/pages/CreateListing/CreateListing.js
+++ front-end/src/pages/CreateListing/CreateListing.js
@@ -75,13 +75,11 @@ const CreateListing = props => {
           />
           <ImageListItemBar
             title={item.title}
-            subtitle={item.author}
             actionIcon={
               <IconButton
                 sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
                 aria-label={`info about ${item.title}`}
               >
-                <InfoIcon />
               </IconButton>
             }
           />
@@ -151,7 +149,6 @@ const CreateListing = props => {
     {
       img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
       title: 'Breakfast',
-      author: '@bkristastucchio',
       rows: 2,
       cols: 2,
       featured: true,
@@ -159,29 +156,24 @@ const CreateListing = props => {
     {
       img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
       title: 'Burger',
-      author: '@rollelflex_graphy726',
     },
     {
       img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
       title: 'Camera',
-      author: '@helloimnik',
     },
     {
       img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
       title: 'Coffee',
-      author: '@nolanissac',
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
       title: 'Hats',
-      author: '@hjrc33',
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
       title: 'Honey',
-      author: '@arwinneil',
       rows: 2,
       cols: 2,
       featured: true,
@@ -189,12 +181,10 @@ const CreateListing = props => {
     {
       img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
       title: 'Basketball',
-      author: '@tjdragotta',
     },
     {
       img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
       title: 'Fern',
-      author: '@katie_wasserman',
     },
   ];
   

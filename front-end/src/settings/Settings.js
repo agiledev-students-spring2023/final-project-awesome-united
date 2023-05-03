9863b5a5db30173a62e08ac9763b233b4711b162
ian41284128 <ian4128@nyu.edu>
2023-05-03 03:27:24 -0400

    fixed public url

--- front-end/src/settings/Settings.js
+++ front-end/src/settings/Settings.js
@@ -1,5 +1,5 @@
 const environment = {
-    "backendBaseUrl": "http://localhost:3001",
-    //"backendBaseUrl": "http://165.227.253.44:3001",
+    //"backendBaseUrl": "http://localhost:3001",
+    "backendBaseUrl": "http://165.227.253.44:3001",
 }
 export default environment

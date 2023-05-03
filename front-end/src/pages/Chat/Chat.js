8c1be8eabdfa4b64483aa533d579e06edec11760
David Acevedo <acevedodavida16@gmail.com>
2023-05-03 05:12:54 -0400

    added full-backend functionality

--- front-end/src/pages/Chat/Chat.js
+++ front-end/src/pages/Chat/Chat.js
@@ -1,180 +1,162 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom";
 import "./Chat.css"
+import { TrySharp } from "@mui/icons-material";
+import  { format }  from  "timeago.js";
+import io from "socket.io-client"
 
 
 
 const Chat = props => {
 
-  // create state variables and their setters so everytime thier value changes, the component updates them in the browser
-  const [message, setMessage] = useState("")
-  const [error, setError] = useState("")
+  const [newMessage, setNewMessage] = useState("") // buffer for user's typed in message
+  const [arrivalMessage, setArrivalMessage] = useState(null)
+  const [otherUser, setOtherUser] = useState("")
+  const [messages, setMessages] = useState([])
+  const socket = useRef()
+  const scroll = useRef()
 
-  useEffect(() => {
-    // this function runs once when the component first loads and again whenever the value of the breed variable changes
-    // check your browser's Developer Tools -> Javascript Console to see this printed when the component first loads and anytime the breed value changes
-    console.log(message)
-  }, [message]) // the variable name in the array here is what causes this function to re-run any time the value of breed changes
-
-  // info on the match ie other person's account
-  const matchInfo = {
-    id: 2,
-    match: "maybe",
-    profileImg: "someicon.png",
-
-  }
-  //an array of chats... imagine this is fetched from a back-end server API
-  // we hard-code it here so we can focus on React.js, not back-end code
-  const chatlog = [
-    {
-      // name of the match individual
-      name: "me",
-      message: "Hi Michael, is this listing still available?",
-      date:"2012-04-23T18:25:43.511Z",
-      // unique ID of the match individual
-      id:0,
-    },
-    {
-      name: "other",
-      message: "Hey, yes it is!",
-      date:"2012-04-23T18:25:59.511Z",
-      id:2,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-    {
-      name: "me",
-      message: "great! can we negotiate?",
-      date:"2012-04-23T18:26:15.511Z",
-      id:0,
-    },
-
-
-  ]
-
-  function whoSent(id){
-    if (id == 0) return "self"
-    else return "other"
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault() // prevent the default browser form submission stuff
-
-    // send the data of the new puppy to a server
-    // this server doesn't exist, so we will see an error in the console
-    // axios' get() and post() methods return a promise, so we can use our javascript Promise or async/await expertise here to deal with the resolution or rejection of the request
-    axios
-      .post("https://someserversomehwere.com/puppy/save", {
-        message: message,
+  useEffect(()=>{
+    socket.current = io("ws://localhost:5000") 
+    socket.current.on("getMessage",data =>{
+      setArrivalMessage({
+        sender: data.senderId,
+        text: data.text,
+        createdAt: Date.now()
       })
-      .then(response => {
-        // success
-        console.log(`Received server response: ${response.data}`)
-      })
-      .catch(err => {
-        // failure
-        console.log(`Received server error: ${err}`)
-        setError(
-          "This form doesn't actually work, sorry.  There is no back-end for this example app in which to save the data. Pop open your web browser's Javascript Console to see the error trying to connect to a non-existent back-end."
-        )
-      })
-      // clear form
-      setMessage('')
-  }
-
-
-  useEffect( () =>{
-    const getMessages = async () => {
-      const res = axios.get( "/messages/"+ "currentMatch" )
-    }
+    })
   },[])
 
-    return (
-      <main>
-        <header>
-            <h2>1653 E 8th St, Brooklyn, NY</h2>
-            <h3>Michael Angelo</h3>
-        </header>
-        <section className="messageBox">
-        {/*
-          * loop through the array of chatlog  data, and display its messages
-          */}
-        {chatlog.map((mes, i, matchesArray) => (
-          <p className={whoSent(mes.id)}>{mes.message}</p>
-        ))}
-        </section>
 
-        {/* input box */}
-        <form id="messageForm" onSubmit={handleSubmit}>
-          <textarea 
-          type="text"
-          placeholder="Message..."
-          onChange={e => setMessage(e.target.value)}
-          value={message}
-          />
-          <input type="submit" id="send-button" value="Send"/>
-        </form>
-      </main>
-    )
+  useEffect(()=>{
+    // if arrivalMessage is not null
+    arrivalMessage && props.match?.members.includes(arrivalMessage.sender) &&
+    setMessages( (prev) => [...prev,arrivalMessage])
+  },[arrivalMessage, props.match])
+
+  useEffect(()=>{
+    socket.current.emit("addUser", props.currentUser.id)
+  },[ props.currentUser ])
+
+  useEffect( ()=> {
+    // UNCOMMENT the following:
+    //setMessages(props.messages)
+    const dummydata = [
+      {
+        matchId: props.match.id,
+        sender: props.otherUserId,
+        text: "Hey mikey",
+      },
+      {
+        matchId: props.match.id,
+        sender: props.otherUserId,
+        text: "a que hora es el bbq",
+      },
+    ]
+    setMessages(dummydata)
+  },[])
+
+
+  useEffect(()=>{
+    const otherId = props.otherUserId
+    // actually get the data associated with that userId
+    const getUser = async ()=>{
+      try{
+        const res = await axios("/User?userId="+otherId)
+        setOtherUser(res.data)
+        console.log(res)
+      }catch(err){
+        console.log(err)
+        // REMOVE WHEN DONE
+        // essentially the user model but stripped down
+        const backupMatch = 
+          {
+          id: otherId,
+          userName: "elapechao",
+          firstName: "Jose",
+          lastName: "Gomez",
+          email: "jgomez@mail.pr",
+          profilePhoto: null
+          }
+        ;
+        setOtherUser(backupMatch)
+        console.log("OVA HERE INGLAND")
+        console.log(otherUser[0].firstName)
+        console.log(backupMatch)
+      }
+    };
+    getUser()
+  },[])
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault() // prevent the default browser form submission stuff
+
+    const message = {
+      matchId: props.match.id,
+      sender: props.currentUser.id,
+      text: newMessage,
+    }
+
+    socket.current.emit("sendMessage", {
+      senderId: props.currentUser.id,
+      receiverId: props.otherUserId,
+      text: newMessage,
+    })
+
+    try{
+      const res = await axios.post("/Chat", message)
+      setMessages([...messages, res.data])
+      setNewMessage("")
+    }catch(err){
+      console.log(err)
+      // delete later:
+      setMessages([...messages, message])
+      console.log(messages)
+      setNewMessage("")
+    }
   }
+
+  useEffect(()=>{
+    scroll.current?.scrollIntoView({behavior:"smooth"})
+  },[messages])
+
+  function whoSent(sender){
+    if (sender == props.currentUser.id) return "self"
+    return "other"
+  }
+
+  return (
+    <>
+    <div className="main">
+      <header>
+          <h2>1653 E 8th St, Brooklyn, NY</h2>
+          <h3>{otherUser.firstName + " " + otherUser.lastName}</h3>
+      </header>
+
+      <div className="messageBox">
+      {/*
+        * loop through the array of messages, and display it
+        */} 
+      {messages.map((message, i, matchesArray) => (
+          <p ref={scroll} className={whoSent(message.sender)}>{message.text}</p>
+      ))}
+      </div>
+      
+      {/* input box */}
+      <form className="messageForm" onSubmit={handleSubmit}>
+        <textarea 
+        type="text"
+        placeholder="Message..."
+        onChange={e => setNewMessage(e.target.value)}
+        value={newMessage}
+        />
+        <input type="submit" id="send-button" value="Send"/>
+      </form>
+      </div>
+      </>
+  )
+}
   
-  export default Chat
+export default Chat
--- socket/index.js
+++ socket/index.js
@@ -4,6 +4,33 @@ const io = require("socket.io")(5000,{
     },
 })
 
+let users = []
+
+
+const addUser = (userId,socketId) =>{
+    //if userId already in 'users' dont add, else do add the user
+    !users.some(user=>user.userId === userId ) &&
+        users.push({userId,socketId})
+}
+
+const getUser = (userId) =>{
+    return users.find(user=>user.userId === userId)
+}
+
 io.on("connection",(socket) =>{
     console.log("user connect")
+    //get user id and socket id from user
+    socket.on("addUser",userId=>{
+        addUser(userId,socket.id)
+        io.emit("getUsers",users)
+    }) 
+
+    // send, retrieve message
+    socket.on("sendMessage",({senderId,receiverId,text}) => {
+        const user = getUser(receiverId)
+        io.to(user.socketId).emit("getMessage",{
+            senderId,
+            text,
+        })
+    })
 })
